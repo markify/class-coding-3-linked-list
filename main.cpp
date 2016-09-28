ad5c5db8502e48fcd3c54a4c17cf439aba3acfac
jerbear <jbayani318_@hotmail.com>
2016-09-28 16:41:35 -0700

    display function edited #3

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ void insertFront(struct Node **head, int n) {
  */
 void display(struct Node *head) {
   //TODO
-  node *temp=head;
+  Node *temp=head;
   while(temp!=NULL){
     cout << temp->data;
     if(temp->next!=NULL){
