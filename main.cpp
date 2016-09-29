172a6151aca5e878b027f27863379142d9c7cb1d
JiawenZhu <zhujiawen519@gmail.com>
2016-09-28 22:19:11 -0700

    addNode(..) new update(it's working).

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 
 struct Node {
-  int data;
-  Node *next;
+    int data;
+    Node *next;
 };
 
 /**
@@ -15,10 +15,10 @@ struct Node {
  * @param head - pointer to the head of the list
  * @param n - data that the first element should hold
  */
-  void initNode(struct Node *head, int n) {
-  //TODO: Initiliaze the node data to n and the next pointer to..?
-  head->data = n;
-  head->next =NULL;
+void initNode(struct Node *head, int n) {
+    //TODO: Initiliaze the node data to n and the next pointer to..?
+    head->data = n;
+    head->next = NULL;
 }
 
 /**
@@ -27,11 +27,11 @@ struct Node {
  * @param n - data that the new node should hold
  */
 void addNode(struct Node *head, int n) {
-  //TODO - Implement this function.
     //TODO - Implement this function.
-    Node *current ;
+    //TODO - Implement this function.
+    Node *current;
     current = head;
-    while (current->next != NULL){
+    while (current->next != NULL) {
         current = current->next;
     }
 
@@ -48,13 +48,13 @@ void addNode(struct Node *head, int n) {
  * @param n - data that the new node should hold
  */
 void insertFront(struct Node **head, int n) {
-  //TODO
-  Node *current;
-  current = *head;
-  Node *newNode;
-  newNode -> next = current;
-  newNode -> data = n;
-  *head = newNode;
+    //TODO
+    Node *current;
+    current = *head;
+    Node *newNode;
+    newNode->next = current;
+    newNode->data = n;
+    *head = newNode;
 }
 
 /**
@@ -62,14 +62,14 @@ void insertFront(struct Node **head, int n) {
  * @param head  - pointer to the head of the list
  */
 void display(struct Node *head) {
-  //TODO
-  Node *temp=head;
-  while(temp){
-    cout << temp->data << " ";
-    temp=temp->next;
+    //TODO
+    Node *temp = head;
+    while (temp) {
+        cout << temp->data << " ";
+        temp = temp->next;
     }
-  cout << endl;
-  cout << endl;
+    cout << endl;
+    cout << endl;
 }
 
 /**
@@ -79,14 +79,14 @@ void display(struct Node *head) {
  * @return - pointer to the node found.
  */
 struct Node *searchNode(struct Node *head, int n) {
-  //TODO
-  Node *thisNode = head;         //first location in front of node
-  while(thisNode) {              //go to current node
-    if(thisNode->data == n)     //if given data is at this node, return
-      return thisNode;
-    else                        // else go to next node
-    thisNode = thisNode->next;
-  }
+    //TODO
+    Node *thisNode = head;         //first location in front of node
+    while (thisNode) {              //go to current node
+        if (thisNode->data == n)     //if given data is at this node, return
+            return thisNode;
+        else                        // else go to next node
+            thisNode = thisNode->next;
+    }
 }
 
 /**
@@ -96,91 +96,90 @@ struct Node *searchNode(struct Node *head, int n) {
  * @return - boolean that indicates if the deletion was successful
  */
 bool deleteNode(struct Node **head, Node *ptrDel) {
-  //TODO
-  return false;
+    //TODO
+    return false;
 }
 
 /* reverse the list */
 struct Node *reverse(struct Node **head) {
-  Node *parent = *head;
-  Node *me = parent->next;
-  Node *child = me->next;
+    Node *parent = *head;
+    Node *me = parent->next;
+    Node *child = me->next;
 
-  /* make parent as tail */
-  parent->next = NULL;
-  while (child) {
+    /* make parent as tail */
+    parent->next = NULL;
+    while (child) {
+        me->next = parent;
+        parent = me;
+        me = child;
+        child = child->next;
+    }
     me->next = parent;
-    parent = me;
-    me = child;
-    child = child->next;
-  }
-  me->next = parent;
-  *head = me;
-  return *head;
+    *head = me;
+    return *head;
 }
 
 /* Creating a copy of a linked list */
 void copyLinkedList(struct Node *node, struct Node **pNew) {
-  if (node != NULL) {
-    *pNew = new Node;
-    (*pNew)->data = node->data;
-    (*pNew)->next = NULL;
-    copyLinkedList(node->next, &((*pNew)->next));
-  }
+    if (node != NULL) {
+        *pNew = new Node;
+        (*pNew)->data = node->data;
+        (*pNew)->next = NULL;
+        copyLinkedList(node->next, &((*pNew)->next));
+    }
 }
 
 /* Compare two linked list */
 /* return value: same(1), different(0) */
 int compareLinkedList(struct Node *node1, struct Node *node2) {
-  static int flag;
+    static int flag;
 
-  /* both lists are NULL */
-  if (node1 == NULL && node2 == NULL) {
-    flag = 1;
-  }
-  else {
-    if (node1 == NULL || node2 == NULL)
-      flag = 0;
-    else if (node1->data != node2->data)
-      flag = 0;
-    else
-      compareLinkedList(node1->next, node2->next);
-  }
+    /* both lists are NULL */
+    if (node1 == NULL && node2 == NULL) {
+        flag = 1;
+    } else {
+        if (node1 == NULL || node2 == NULL)
+            flag = 0;
+        else if (node1->data != node2->data)
+            flag = 0;
+        else
+            compareLinkedList(node1->next, node2->next);
+    }
 
-  return flag;
+    return flag;
 }
 
 void deleteLinkedList(struct Node **node) {
-  struct Node *tmpNode;
-  while (*node) {
-    tmpNode = *node;
-    *node = tmpNode->next;
-    delete tmpNode;
-  }
+    struct Node *tmpNode;
+    while (*node) {
+        tmpNode = *node;
+        *node = tmpNode->next;
+        delete tmpNode;
+    }
 }
 
 int main() {
-  struct Node *newHead;
-  struct Node *head = new Node;
+    struct Node *newHead;
+    struct Node *head = new Node;
 
 
-  initNode(head,10);
-  display(head);
+    initNode(head, 10);
+    display(head);
 
-  addNode(head,20);
-  display(head);
+    addNode(head, 20);
+    display(head);
 
-  addNode(head,30);
-  display(head);
+    addNode(head, 30);
+    display(head);
 
-  addNode(head,35);
-  display(head);
+    addNode(head, 35);
+    display(head);
 
-  addNode(head,40);
-  display(head);
+    addNode(head, 40);
+    display(head);
 
-  insertFront(&head,5);
-  display(head);
+    insertFront(&head, 5);
+    display(head);
 /*
   int numDel = 5;
   Node *ptrDelete = searchNode(head,numDel);
@@ -223,5 +222,5 @@ int main() {
   deleteLinkedList(&newHead);
   display(newHead);
    */
-  return 0;
+    return 0;
 }
